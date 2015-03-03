0cebed2f8d70f7fb81807235bcb6e30fcfbe9820
matheusffer <fmatheus42@live.com>
2015-03-03 20:45:36 -0300

    Enviando arquivos

---
+++ AulaHelloWorld/src/br/unibh/Aluno.java
@@ -0,0 +1,5 @@
+package br.unibh;
+
+public class Aluno {
+
+}
---
+++ AulaHelloWorld/src/br/unibh/HelloWorld.java
@@ -0,0 +1,13 @@
+package br.unibh;
+
+import javax.swing.*;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Ola mundo!");
+JOptionPane.showMessageDialog(null, "Oi mundo");
+
+	}
+
+}
